6c25fcc09f6b1f627a28c2977ee44815941b502e
daria gulchak <152175423+gulchak-d@users.noreply.github.com>
2025-11-21 21:35:00 +0200

    Create app_tests.py

---
+++ app_tests.py
@@ -0,0 +1,12 @@
+import unittest
+import app as tested_app
+import xmlrunner
+
+class TestApp(unittest.TestCase):
+    def test_hello_world(self):
+        self.assertEqual(tested_app.hello_world(), 'Hello World!')
+
+if __name__ == '__main__':
+    runner = xmlrunner.XMLTestRunner(output='test-reports')
+    unittest.main(testRunner=runner)
+    unittest.main()
