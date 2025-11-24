b82cfa0528db25ff19470e04cb99bb758389ea87
daria gulchak <152175423+gulchak-d@users.noreply.github.com>
2025-11-24 11:49:21 +0200

    Update app_tests.py

--- app_tests.py
+++ app_tests.py
@@ -4,7 +4,7 @@ import xmlrunner
 
 class TestApp(unittest.TestCase):
     def test_hello_world(self):
-        self.assertEqual(tested_app.hello_world(), 'Hello World!')
+        self.assertEqual(tested_app.hello_world(), 'Hello new day')
 
 if __name__ == '__main__':
     runner = xmlrunner.XMLTestRunner(output='test-reports')
